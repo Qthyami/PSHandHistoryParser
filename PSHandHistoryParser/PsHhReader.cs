3b9752d59ac8385db584d75910943d3f95e7e170
Qthyami <113956492+Qthyami@users.noreply.github.com>
2026-01-07 05:19:37 +0300

    cosmetic fix

--- PSHandHistoryParser/PsHhReader.cs
+++ PSHandHistoryParser/PsHhReader.cs
@@ -15,18 +15,15 @@ namespace PSHhParser
             var hands = new List<string>();
             var currentHand = new StringBuilder();  //временная переменная, очищаемая к концу кажого цикла после добавления в переменныю hands
 
-
             bool startHand = false;  //флаги, изначально зануленные и заполняемые по мере продвижения к концу раздачи. 
             bool summary = false;
             bool totalPotLine = false;
             bool summaryCompleted = false;
             int seatLineCointer = 0; //сколько строк seat уже прошли
-
-
+            
             foreach (var line in File.ReadLines(path))
             {
                 if (string.IsNullOrEmpty(line)) continue;
-
 
                 if (line.StartsWith("PokerStars Hand #"))   // если встречаем начало раздачи и все флаги заполнены обрезаем заполение currentHand и пушим содержимое в hands
                 {
@@ -34,7 +31,6 @@ namespace PSHhParser
                     {
                         hands.Add(currentHand.ToString().Trim());
                         currentHand.Clear();
-
                     }
                     startHand = true;
                     summary = false;
@@ -42,8 +38,6 @@ namespace PSHhParser
                     seatLineCointer = 0;
                     currentHand.AppendLine(line);
                     continue;
-
-
                 }
 
                 if (startHand)  //мы внутри раздачи добавляем строка за строкой в currentHand
@@ -51,7 +45,6 @@ namespace PSHhParser
                     currentHand.AppendLine(line);
                 }
                 else continue; // Добавляем строки только после начала раздачи, остальное игнорируем
-
 
                 if (line.StartsWith("*** SUMMARY ***")) //дальше по мере заполения флагов подходим к концу разадчи и когда опять встретим "PokerStars Hand #" обрезаем чтение
                 {
@@ -70,27 +63,18 @@ namespace PSHhParser
                 if (summary && totalPotLine && line.StartsWith($"Seat "))
                 {
                     seatLineCointer++;
-
                     if (seatLineCointer >= 2 && seatLineCointer <= 10)
                     {
                         summaryCompleted = true;
-
                     }
-
-
                 }
-
             }
 
             if (startHand && summaryCompleted)  // случай конца txt файла - пушим в List накопленное, хоть и не встретили начало новой руки
             {
-
                 hands.Add(currentHand.ToString().Trim());
             }
-
             return hands;
         }
-
-
     }
 }
